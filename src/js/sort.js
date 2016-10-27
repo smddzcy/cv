4b6b4b1cdba895f4cec388b07803a2d0c2ac3f17
Gina <gina@oneilinteractive.com>
2016-10-27 11:50:16 -0400

    Added Sorting

---
+++ src/js/sort.js
@@ -0,0 +1,28 @@
+$( function() {
+    $( "#sort-language" ).sortable();
+    $( "#sort-language" ).disableSelection();
+    
+    $( "#sort-honors" ).sortable();
+    $( "#sort-honors" ).disableSelection();
+    
+    $( "#sort-skill" ).sortable();
+    $( "#sort-skill" ).disableSelection();
+    
+    $( "#sort-experience" ).sortable();
+    $( "#sort-experience" ).disableSelection();
+    
+    $( "#sort-education" ).sortable();
+    $( "#sort-education" ).disableSelection();
+ 
+    $( "#sort-opensource" ).sortable();
+    $( "#sort-opensource" ).disableSelection();
+   
+    $( "#sort-certifications" ).sortable();
+    $( "#sort-certifications" ).disableSelection();
+ 
+    $( "#sort-projects" ).sortable();
+    $( "#sort-projects" ).disableSelection();
+ 
+    $( "#sort-volunteer" ).sortable();
+    $( "#sort-volunteer" ).disableSelection();
+});
